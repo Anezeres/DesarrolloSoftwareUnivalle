19d59006ca98a533116020bfd9fa4684ae910a96
marcela981 <marcela.mazo@correounivalle.edu.co>
2023-06-13 20:47:34 -0500

    Dashboard-Sidebar
    V2.0.1.0

--- client/front-green-wheels/src/pages/Frontend/Dashboard-Admin/dash-ad/src/comoponents/sidebar.js
+++ client/front-green-wheels/src/pages/Frontend/Dashboard-Admin/dash-ad/src/comoponents/sidebar.js
@@ -1,23 +1,25 @@
 import { Link } from 'react-router-dom' 
+import * as FaIcons from 'react-icons/fa'
+
 
 const Sidebar = () => {
     return (
         <div className="sidebar">
             <ul>
                 <li>
-                    <Link to="/"> Inicio </Link>
+                    <Link to="/" style={{ color: '#C0D860' }}> <FaIcons.FaHome className='me-2'/> Inicio </Link>
                 </li>
 
                 <li>
-                    <Link to="/Sales"> Informes ventas </Link>
+                    <Link to="/Sales" style={{ color: '#C0D860' }}> <FaIcons.FaRegChartBar className='me-2'/> Informes ventas </Link>
                 </li>
 
                 <li>
-                    <Link to="/Clients"> Informes clientes </Link>
+                    <Link to="/Clients" style={{ color: '#C0D860' }}> <FaIcons.FaUserCircle className='me-2'/> Informes clientes </Link>
                 </li>
-
+                
                 <li>
-                    <Link to="/Users"> Usuarios </Link>
+                    <Link to="/Users" style={{ color: '#C0D860' }}> <FaIcons.FaUsersCog className='me-2'/> Usuarios </Link>
                 </li>
 
             </ul>
