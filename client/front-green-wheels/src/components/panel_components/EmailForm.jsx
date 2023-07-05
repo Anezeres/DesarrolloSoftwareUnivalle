5d74bd5cd16e0db8694557bdbc19de62df9133bf
Nicol Ortiz <nicolvaleria0919@gmail.com>
2023-07-05 13:28:17 -0500

    comments were removed

--- client/front-green-wheels/src/components/panel_components/EmailForm.jsx
+++ client/front-green-wheels/src/components/panel_components/EmailForm.jsx
@@ -11,39 +11,6 @@ export const EmailForm = () =>{
     const [mensaje_exitoso, setMensaje_exitoso] = useState(false)
     const [correos_destinatarios2, setCorreosDestinatarios] = useState('');
 
-    // const enviarTodos = (url) => {
-    //     axios.get(url)
-    //       .then(response => {
-    //         const correosDestinatarios = [];
-    //         response.data.forEach(item => {
-    //             const correo = item[0];
-    //             correosDestinatarios.push(correo);
-    //           });
-    //           setCorreosDestinatarios(correosDestinatarios.join(', '));
-    //       })
-    //       .catch(error => {
-    //         console.error(error);
-    //       });
-    //   };
-
-        // const enviarEmpleadoEspecifico = (url,grupo) => {
-        //     axios.get(url)
-        //     .then(response => {
-        //         const correosDestinatarios = [];
-        //         response.data.forEach(item => {
-        //             const correo = item[0];
-        //             const grupos = item[1];
-        //             if (grupos.includes(grupo)) {
-        //             correosDestinatarios.push(correo);
-        //             }
-        //         });
-        //         setCorreosDestinatarios(correosDestinatarios.join(', '));
-        //     })
-        //     .catch(error => {
-        //         console.error(error);
-        //     });
-        // };
-
     return (
         <div className='d-flex justify-content-end'>
             <Formik 
@@ -123,69 +90,7 @@ export const EmailForm = () =>{
                     </Form>
                 )}    
             </Formik>
-            {/* <div className="mx-auto">
-                <CButton color="primary" onClick={() => enviarTodos('http://localhost:8000/get_employees_email')}>Todos</CButton>
-                <CButton color="primary" onClick={() => enviarEmpleadoEspecifico('http://localhost:8000/get_employees_email',3)}>Jefes de taller</CButton> 
-                <CButton color="primary" onClick={() => enviarEmpleadoEspecifico('http://localhost:8000/get_employees_email',2)}>Vendedores</CButton> 
-            </div>             */}
 		</div>
     );
 }
 
-{/* touched detecta cuando se ha tocado un input, su valor cambia a True */}
-                {/* {( {values, errors, touched, handleSubmit, handleChange, handleBlur} ) => (
-                    <form className="formulario" onSubmit={handleSubmit}>
-                        <div>
-                            <label htmlFor="correo_remitente">Correo remitente</label>
-                            <input 
-                            type="email" 
-                            id='correo_remitente' 
-                            name='correo_remitente' 
-                            placeholder="Ingrese su correo" 
-                            value={values.correo_remitente}
-                            onChange={handleChange}
-                            onBlur={handleBlur}
-                            />
-                            {touched.correo_remitente && errors.correo_remitente && <div className='error'>{errors.correo_remitente}</div>}
-                        </div>
-                        <div>
-                            <label htmlFor="correo_destinatario">Correo destinatario</label>
-                            <input 
-                            type="email" 
-                            id='correo_destinatario' 
-                            name='correo_destinatario' 
-                            placeholder="Ingrese el correo a quien va dirigido" 
-                            value={values.correo_destinatario}
-                            onChange={handleChange}
-                            onBlur={handleBlur}
-                            />
-                            {touched.correo_destinatario && errors.correo_destinatario && <div className='error'>{errors.correo_destinatario}</div>}
-                        </div>
-                        <div>
-                            <label htmlFor="asunto">Asunto</label>
-                            <input 
-                            type="text" 
-                            id='asunto' 
-                            name='asunto' 
-                            placeholder="Ingrese el asunto" 
-                            value={values.asunto}
-                            onChange={handleChange}
-                            onBlur={handleBlur}
-                            />
-                        </div>
-                        <div>
-                            <label htmlFor="mensaje">Mensaje</label>
-                            <textarea 
-                            type="text" 
-                            id='mensaje' 
-                            name='mensaje' 
-                            placeholder="Escribe el mensaje" 
-                            value={values.mensaje}
-                            onChange={handleChange}
-                            onBlur={handleBlur}
-                            />
-                        </div>
-                        <button type="submit">Enviar</button>
-                        {mensaje_exitoso && <p className='exito'>Formulario enviado con éxito!</p>}
-                    </form> 
-                )} */}
